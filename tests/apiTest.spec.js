b46620ec7d8eb8054eca70db96d58926f0284d20
sagar_6502 <sagarsunar202@gmail.com>
2024-10-24 18:02:51 +0530

    GET method

---
+++ tests/apiTest.spec.js
@@ -0,0 +1,72 @@
+const { test, expect } = require('@playwright/test');
+// import generateRandomNumber from "../util/randomGenerator";
+
+const baseURI = 'https://fakestoreapi.com/products/';
+
+function generateRandomNumber(){
+
+    const data = parseInt(Math.random()*10+1);
+    // console.log(data);
+    return data;
+}
+
+test.only("get test", async ({page}) => {
+   const data = generateRandomNumber();
+    console.log(data);
+    const val = fetch(baseURI+data)
+        .then(res=>res.json())
+        .then(json=>console.log(json));
+
+    expect(val).toHaveLength;
+});  
+
+test("POST test", async ({page}) => {
+   
+    const data = parseInt(Math.random()*10);
+    console.log(data);
+    const val =fetch('https://fakestoreapi.com/products',{
+        method:"POST",
+        body:JSON.stringify(
+            {
+                title: 'test product',
+                price: 13.5,
+                description: 'lorem ipsum set',
+                image: 'https://i.pravatar.cc',
+                category: 'electronic'
+            }
+        )
+    })
+        .then(res=>res.json())
+        .then(json=>console.log(json))
+    expect(val).toHaveLength;
+}); 
+
+test("PUT test", async ({page}) => {
+   
+    console.log(Math.random()*10);
+    const val = fetch('https://fakestoreapi.com/products/7',{
+        method:"PUT",
+        body:JSON.stringify(
+            {
+                title: 'test product',
+                price: 13.5,
+                description: 'lorem ipsum set',
+                image: 'https://i.pravatar.cc',
+                category: 'electronic'
+            }
+        )
+    })
+        .then(res=>res.json())
+        .then(json=>console.log(json))
+    expect(val).toHaveLength
+}); 
+
+test("DELETE test", async ({page}) => {
+   
+    const val = fetch('https://fakestoreapi.com/products/6',{
+        method:"DELETE"
+    })
+        .then(res=>res.json())
+        .then(json=>console.log(json))
+    expect(val).toHaveLength;
+}); 
---
+++ util/randomGenerator.js
@@ -0,0 +1,7 @@
+
+export default function generateRandomNumber(){
+
+    const data = parseInt(Math.random()*10);
+    // console.log(data);
+    return data;
+}
